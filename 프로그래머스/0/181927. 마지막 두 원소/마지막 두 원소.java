e8c2f9398c5e5409d31bfefd5d29c768e2b73afc
yejin <76733462+blue032@users.noreply.github.com>
2025-04-16 10:41:52 +0900

    [level 0] Title: 마지막 두 원소, Time: 3.97 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,22 +1,25 @@
 import java.util.List;
 import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> answer = new ArrayList<>();
-        for(int i = 0; i< num_list.length;i++){
-            if(i == num_list.length - 1){
-                answer.add(num_list[i]);
-                if(num_list[i-1] < num_list[i]){
-                    answer.add(num_list[i] - num_list[i-1]);
+        //리스트로 만들어야 원소들을 자유롭게 추가, 삭제 가능하다
+        List<Integer> answer = new ArrayList<>(); 
+        
+        for (int i = 0; i < num_list.length; i++) {
+            answer.add(num_list[i]);
+            if (i == num_list.length - 1) {
+                
+                if (num_list[i - 1] < num_list[i]) {
+                    answer.add(num_list[i] - num_list[i - 1]);
+                } else {
+                    answer.add(num_list[i] * 2);
                 }
-                else{
-                    answer.add(num_list[i]*2);
-                }
-            }
-            else{
-                answer.add(num_list[i]);
-            }
+             } //else {
+            //     answer.add(num_list[i]);
+            // }
         }
+        
         return answer.stream().mapToInt(i->i).toArray();
     }
 }
