f8ba97d1876d63ccc06c5441e662e8d44f08af18
yejin <76733462+blue032@users.noreply.github.com>
2025-06-03 21:26:51 +0900

    [Silver IV] Title: 수 찾기, Time: 628 ms, Memory: 46072 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] cards = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            cards[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(cards);
+
+        int m = Integer.parseInt(br.readLine());
+        String[] checkInput = br.readLine().split(" ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(checkInput[i]);
+            if (binarySearch(cards, target)) {
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static boolean binarySearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == key) return true;
+            else if (arr[mid] < key) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+}
